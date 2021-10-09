d8ba7dc2bf73d5b4ecd14cc43fe457b18541f799
Oscar Wihlborg Ergun <oscar.w.ergun@gmail.com>
2021-10-10 00:26:06 +0200

    answer the question 1-5

--- functions/exercise.js
+++ functions/exercise.js
@@ -19,13 +19,38 @@
 // Exercise -3
 /*Create a function that takes the users age in years and returns the age in days.*/
 
-let userAge= prompt("Enter your age: ");
-function ageinDays (userAge) {
-    let result = userAge * 360;
-    return result;
-}
-console.log(ageinDays(userAge));
+// let userAge= prompt("Enter your age: ");
+// function ageinDays (userAge) {
+    // let result = userAge * 360;
+    // return result;
+// }
+// console.log(ageinDays(userAge));
 
 // Exercise -4 
 /* Write a function that takes the base and height of a triangle and return its area. The area of a triangle is: (base * height) / 2
 */
+
+// function expression
+const calcArea = function(base, height) {
+    return (base * height) /2;
+
+};
+const area = calcArea(5,6);
+console.log(area);
+
+// Exercise-5
+/* Write a function that converts days into minutes.
+*/
+
+// const makeMin = function(days) {
+    // return (days * 24) * 60;
+// };
+// console.log(makeMin(3));
+
+
+// arrow function obs du kan ta bort return om du har bara en return value
+const makeMin = (days) => {
+    return (days * 24) *60;
+};
+const minutes = makeMin(3);
+console.log(minutes);
