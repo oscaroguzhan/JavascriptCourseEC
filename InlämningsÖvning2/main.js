91f5cf92c66e6e2d21b5da3e0cc3c607d9fc7f6b
Oscar Wihlborg Ergun <oscar.w.ergun@gmail.com>
2021-11-16 11:39:39 +0100

    in modification process

--- InlämningsÖvning2/main.js
+++ InlämningsÖvning2/main.js
@@ -4,28 +4,47 @@ let addButton = document.querySelector("#AddBtn");
 let trackButton = document.querySelector("#FastTrackBtn");
 let checkInButton = document.querySelector("#CheckInBtn");
 let passangerList = document.querySelector("#passangerList");
-let plats = [];
+let passangerArray = [];
 
 // ----------------------------------
 addButton.addEventListener("click", () => {
     let newPassenger = document.createElement("li");
     newPassenger.textContent = inputElement.value;
-    passangerList.appendChild(newPassenger);
-    plats.push(newPassenger);
+    passangerArray.push(newPassenger);
+
+    passangerArray.forEach(passenger => {
+        passenger = newPassenger;
+        passangerList.appendChild(passenger);
+    })
+    console.log(passangerArray[0].innerHTML);
+
 })
 
 // TODO: OPTIMERA SÅ NÄR MAN TRYCKER PÅ TRACK KNAPPEN SÅ 
-trackButton.addEventListener("click", (event) => {
-    event.preventDefault();
+trackButton.addEventListener("click", (e) => {
+    e.preventDefault();
     let newPassenger = document.createElement("li")
     newPassenger.textContent = inputElement.value;
-        passangerList.insertBefore(newPassenger, passangerList.firstChild);
-        passangerList.removeChild(passangerList.lastChild);
+    passangerArray.push(newPassenger);
     
+        // passangerList.removeChild(passangerList.lastChild);
+        // passangerArray.forEach(passenger => {
+        //      passangerList.insertBefore(passenger, passangerList.firstChild);
+        //     passangerList.appendChild(newPassenger);
+        //     // passangerArray.unshift(newPassenger);
+        // })
+        for(let i = 0; i < passangerArray.length; i++) {
+            passangerArray[i].insertBefore(newPassenger, passangerList.firstChild);
+            passangerArray[i].appendChild(newPassenger);
+        }
 })
+
+
  // TODO: Alert vid tom list
 
  checkInButton.addEventListener("click", () => {
     passangerList.removeChild(passangerList.childNodes[0]);
-    plats.push(newPassenger);
+    if(passangerArray.length === 0) {
+        alert("Det finns inga passagerare i kön!");
+    }
 })
