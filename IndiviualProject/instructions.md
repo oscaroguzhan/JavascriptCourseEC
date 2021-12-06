c30256a110c5077cf74473446bec68f5d245db77
oscaroguzhan <72919227+oscaroguzhan@users.noreply.github.com>
2021-12-06 15:43:03 +0100

    initial setup and filter section

---
+++ IndiviualProject/instructions.md
@@ -0,0 +1,15 @@
+Din uppgift är att hantera elever som söker till eftergymnasiala utbildningar och matcha dem med rätt skolor. Ta fram en applikation som underlättar arbetet.
+
+Data för elever: https://api.mocki.io/v2/01047e91/students
+Data för skolor: https://api.mocki.io/v2/01047e91/schools
+
+Kriterier för Godkänt
+
+1.Användaren ska ha möjlighet att se samtliga elever i en lista när sidan laddas.
+2.Användaren ska kunna filtrera eleverna i listan baserat på utbildning.
+3.Användaren ska också kunna sortera listan baserat på följande:
+Ålder (lägst först)
+Förnamn (alfabetisk ordning)
+Efternamn (alfabetisk ordning)
+
+4.Användaren ska kunna välja en elev i listan, och se en lista över samtliga skolor som passar eleven. (En skola passar en elev om dess önskade utbildning finns samt att skolan har en aktivitet som matchar en av elevens hobbys).
---
+++ IndiviualProject/main.js
@@ -0,0 +1,112 @@
+let contentContainer = document.querySelector(".content-container");
+let h3 = document.getElementById("h3");
+let elevList = document.querySelector(".elevList");
+let radioBtnsProgramme = document.querySelectorAll('[name="programme"]');
+let filterBtn = document.getElementById("filter_Btn");
+sortAgeBtn = document.getElementById("age_sort_btn");
+sortFirstNameBtn = document.getElementById("firstName_sort_btn");
+sortLastNameBtn = document.getElementById("lastName_sort_btn");
+
+// get info om student and scholl from API Endpoints
+// first create a async function to fetch data
+let getData = async (URL) => {
+  let response = await fetch(URL);
+  let data = await response.json();
+  return data;
+};
+
+// define a function to choose api endpoints
+async function chooseData() {
+  let students = await getData("https://api.mocki.io/v2/01047e91/students");
+  let schools = await getData("https://api.mocki.io/v2/01047e91/schools");
+
+  let displayNoneH3 = function () {
+    h3.style.display = "none";
+  };
+  students.forEach((student) => {
+    // console.log(`${student.firstName} ${student.lastName}`);  // kontrolutskrift
+    let studentInfo = document.createElement("li");
+    // add textcontent to skapade elemenet
+    studentInfo.innerHTML = `${student.firstName} ${student.lastName}`;
+    // ------------------------------
+
+    // append element into parent
+    elevList.appendChild(studentInfo);
+    contentContainer.appendChild(elevList);
+    // ------------------------------
+  });
+
+  // filter section -----------------
+  filterBtn.addEventListener("click", () => {
+    displayNoneH3();
+
+    // tömma listan
+    elevList.innerHTML = "";
+    radioBtnsProgramme.forEach((btn) => {
+      if (btn.checked) {
+        let programme = btn.value;
+        //console.log(programme); kontrolutskrift
+
+        // filter students based on programme
+        let filteredbyProgramme = students.filter(
+          (student) => student.programme === programme
+        );
+        filteredbyProgramme.forEach((student) => {
+          let filteredStudent = document.createElement("li");
+          filteredStudent.textContent = `${student.firstName} ${student.lastName}`;
+          let newTitel = document.querySelector("#h2");
+          newTitel.innerHTML = `Student som läser ${student.programme} programme`;
+          // append the filtered students to the list
+          elevList.appendChild(filteredStudent);
+        });
+      }
+    });
+  });
+
+  // sort section
+  sortAgeBtn.addEventListener("click", () => {
+    displayNoneH3();
+    elevList.innerHTML = "";
+    // sort the array by age
+    students.sort((student1, student2) =>
+      student1.age > student2.age ? 1 : -1
+    );
+    students.forEach((student) => {
+      let sortedStudentsByAge = document.createElement("li");
+      sortedStudentsByAge.innerHTML = `${student.firstName} ${student.lastName} är <span id="age">${student.age}</span>`;
+      let newTitel = document.querySelector("#h2");
+      newTitel.innerHTML = `Elever sorterad baserad på sin ålder`;
+      elevList.append(sortedStudentsByAge);
+    });
+  });
+
+  sortFirstNameBtn.addEventListener("click", () => {
+    displayNoneH3();
+    elevList.innerHTML = "";
+    students.sort((elev1, elev2) =>
+      elev1.firstName > elev2.firstName ? 1 : -1
+    );
+    students.forEach((student) => {
+      let sortedStudentsByFirstName = document.createElement("li");
+      sortedStudentsByFirstName.innerHTML = ` <strong>${student.firstName}</strong> ${student.lastName}`;
+      let newTitel = document.querySelector("#h2");
+      newTitel.innerHTML = `Elever sorterad baserad på sin förnamn`;
+      elevList.append(sortedStudentsByFirstName);
+    });
+  });
+
+  sortLastNameBtn.addEventListener("click", () => {
+    displayNoneH3();
+    elevList.innerHTML = "";
+    students.sort((elev1, elev2) => (elev1.lastName > elev2.lastName ? 1 : -1));
+    students.forEach((student) => {
+      let sortedStudentsByLastName = document.createElement("li");
+      sortedStudentsByLastName.innerHTML = ` ${student.firstName} <strong>${student.lastName}</strong>`;
+      let newTitel = document.querySelector("#h2");
+      newTitel.innerHTML = `Elever sorterad baserad på sin efternamn`;
+      elevList.append(sortedStudentsByLastName);
+    });
+  });
+}
+
+chooseData();
